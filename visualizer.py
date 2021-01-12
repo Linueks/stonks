eb6a3bb1d08e7b5aea5fd44ad67780698fc2a1fc
Linus Ekstrøm <linueks@gmail.com>
2021-01-12 13:53:42 +0100

    added functionality to logplot and additionally to plot a custom x-day moving average over the plot

--- visualizer.py
+++ visualizer.py
@@ -2,4 +2,36 @@ from parameters_and_imports import *
 
 
 
-def plot_close_price(data):
+df = pd.read_hdf(f'stock_data\\{stock_ticker}.h5')
+
+
+
+def plot_close_price(data, plot_log=False, moving_avg=False):
+    """
+    Assumes a pandas dataframe is passed.
+    """
+
+    plt.title(f'Historical Price {stock_ticker}')
+    plt.xlabel('Date')
+    plt.ylabel('USD ($)')
+
+
+    if plot_log:
+        plt.plot(np.log(data['Close']))
+
+    else:
+        if moving_avg:
+            values = data['Close'].rolling(window=moving_avg,
+                                            center=False).mean()
+            plt.plot(values, label=f'{moving_avg} days moving average')
+        else:
+            pass
+
+
+        plt.plot(data['Close'], label=f'{stock_ticker} price')
+        plt.legend()
+
+
+
+plot_close_price(df, moving_avg=20)
+plt.show()
