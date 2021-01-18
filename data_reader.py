43341dbf3be78799d0ab252f9cf295a1b9bf29a7
Linus Ekstrøm <linueks@gmail.com>
2021-01-18 13:28:24 +0100

    fixed data reading so it actually reads from file, also added promt to notify when update of data is smart

--- data_reader.py
+++ data_reader.py
@@ -1,10 +1,27 @@
 from parameters_and_imports import *
+import os
 
 
 
-def get_stock():
+
+def get_stock(force_update=True):
+
+
+    if force_update:
+        df = web.DataReader(stock_ticker, data_source='yahoo', start=start_date,
+                            end=todays_date)
+        df['Daily Return'] = df['Adj Close'].pct_change()
+        df.to_hdf(f'stock_data\\{stock_ticker}.h5', key='df')
+
+
     try:
         with open(f'stock_data\\{stock_ticker}') as f:
+            time_since = os.path.getmtime(f)
+            time_since_days = np.round(time_since / 8.64e7, 2)
+
+            if time_since_days > 7:
+                print(f'{time_since_days} since last update, consider running\
+                        with force_update=True')
             df = pd.read_hdf(f)
 
     except IOError:
@@ -12,6 +29,18 @@ def get_stock():
                             end=todays_date)
         df['Daily Return'] = df['Adj Close'].pct_change()
         df.to_hdf(f'stock_data\\{stock_ticker}.h5', key='df')
+        print('hallo')
 
     finally:
         return df
+
+
+
+
+if __name__=='__main__':
+    #get_stock()
+
+    with open(f'stock_data\\{stock_ticker}.h5') as f:
+        time_since = os.path.getmtime(f'stock_data\\{stock_ticker}.h5')
+        print(time_since / 8.64e7)
+        print('hei')
