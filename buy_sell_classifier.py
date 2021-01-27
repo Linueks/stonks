2b6222a34611657ff1c6cfb766dceb03964f28a6
Linus Ekstrøm <linueks@gmail.com>
2021-01-27 15:02:25 +0100

    last changes from yesterday

--- buy_sell_classifier.py
+++ buy_sell_classifier.py
@@ -3,7 +3,7 @@ from data_reader import get_stock
 
 
 
-def classify(current, future, ratio=1.03):
+def classify(current, future, ratio=1.02):
     """
     According to ratio tells you if it's a buy 1 or sell 0
     The ratio can be tuned to reflect i.e. percentage taken by bank etc
@@ -15,45 +15,74 @@ def classify(current, future, ratio=1.03):
 
 
 
-def preprocess_df(df):#, training_percentage):
+def preprocess_df(df, verbose=False):#, training_percentage):
     """
     This function wants to take in a given dataframe and then make a new column
-    with features 0 or 1 where 0 represents sell and 1 represents buy.
+    with features 0 or 1 where 0 represents sell and 1 represents buy. This is
+    determined from the variable "future_period_predict". This chooses the day
+    to compare to in order to see if the price went up or down
 
+
+    Should return training and validation data set.
     """
-    df = df.drop('future', 1)
-    #print(df)
 
-    #df = df.drop('Daily Return')
-    #df.dropna(inplace=True)
-
-    #for col in df.columns:
-    #    if col != 'target':
-    #        print(col)
-    #        df[col] = df[col].pct_change()
-    #        df.dropna(inplace=True)
-    #        df[col] = preprocessing.scale(df[col].values)
-
-    #df.dropna(inplace=True)
+    df['future'] = df['Close'].shift(-future_period_predict)
+    df.dropna(inplace=True)
+    df['target'] = list(map(classify, df['Close'], df['future']))               # I feel this can be done cleaner with just dataframe operations
+    #df['target'] = df['Close'].apply()
+    #print(df['target'].value_counts()) #checking if the map worked
+    training_data_length = np.int(np.ceil(len(df['Close'])\
+                                        * training_percentage))
+    #print(training_data_length)
+    #print(df.iloc[60:80])
 
 
-    return df
+    x_train = []
+    y_train = []
+
+
+    for i in range(sequence_length, training_data_length):
+        x_train.append(df['Close'].values[i-sequence_length:i])
+
+
+    y_train = df['target'].values[sequence_length:training_data_length]
+    x_train = np.array(x_train)
+
+    #print(x_train[0].shape, y_train.shape)
+    #print(x_train[0], x_train[1])                                              # Check the sequences in x_train with the corresponding values in
+    #print(y_train[60], y_train[61])                                            # y train, they're shifted by the value sequence length from each other
+
+
+    x_validation = []
+
+    for i in range(training_data_length, len(df['Close'].values)):
+        x_validation.append(df['Close'].values[i-sequence_length:i])
+
+
+    x_validation = np.array(x_validation)
+    y_validation = df['target'].values[training_data_length:]
+
+    train_unique, train_counts = np.unique(y_train, return_counts=True)
+    val_unique, val_counts = np.unique(y_validation, return_counts=True)
+
+    if verbose:
+        print(f'train data: {len(x_train)} validation: {len(x_validation)}')
+        print(f'Dont buys: {train_counts[0]}, buys: {train_counts[1]}')
+        print(f'VALIDATION Dont buys: {val_counts[0]}, buys: {val_counts[1]}')
 
 
 
-#NAME = f"{SEQ_LEN}-SEQ-{FUTURE_PERIOD_PREDICT}-PRED-{int(time.time())}"
+    return x_train, y_train, x_validation, y_validation
 
 
-stock_data = get_stock(stock_ticker)
 
-stock_data['future'] = stock_data['Close'].shift(-future_period_predict)
-print(stock_data)
-
-stock_data.dropna(inplace=True)
-stock_data['target'] = list(map(classify, stock_data['Close'], stock_data['future']))
-print(stock_data)
-print(stock_data['target'].value_counts())
+def recurrent_net():
+    return
 
 
-stock_data = preprocess_df(stock_data)
-#print(stock_data)
+
+
+
+
+stock_data = get_stock(stock_ticker, force_update=True)
+x_train, y_train, x_validation, y_validation = preprocess_df(stock_data)
